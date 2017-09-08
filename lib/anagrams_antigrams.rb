155bb771de06f33b15d51c176467d88bc7352adb
richa.shaurbh <richa.shaurabh@gmail.com>
2017-09-08 11:51:54 -0700

    this will add the code for Checking if two word irrespective of case

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -5,6 +5,17 @@ class AnagramsAntigrams
 		@second_word = second_word
 	end
 	def find_anagrams
-		"These words are anagrams"
+		first_word_arry = @first_word.downcase().split("")
+		second_word_arry = @second_word.downcase().split("")
+		matching_letter_arr = []
+		first_word_arry.each do |letter|
+			if (second_word_arry.include?(letter)) & (first_word_arry.length == second_word_arry.length)
+				matching_letter_arr.push(letter)
+				# binding.pry
+			end
+		end
+		if first_word_arry.length == matching_letter_arr.length
+		 return "These words are anagrams"
+		end
 	end
 end
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -4,12 +4,12 @@ require("pry")
 
 describe("Anagrams_Antilog") do
 	it("Check if two words are anagrams") do
-		anagrams_antigrams_obj = AnagramsAntigrams.new("mom","mom")
+		anagrams_antigrams_obj = AnagramsAntigrams.new("tea","eat")
 		expect(anagrams_antigrams_obj.find_anagrams()).to(eq("These words are anagrams"))
 	end
 
-	# it("Account for the possibility that words might have different cases but should still be anagrams") do
-	# 	anagrams_antigrams_obj = AnagramsAntigrams.new("mom","mom")
-	# 	expect(anagrams_antigrams_obj.find_anagrams()).to(eq("These words are anagrams"))
-	# end
+	it("Account for the possibility that words might have different cases but should still be anagrams") do
+		anagrams_antigrams_obj = AnagramsAntigrams.new("Tea","Eat")
+		expect(anagrams_antigrams_obj.find_anagrams()).to(eq("These words are anagrams"))
+	end
 end
